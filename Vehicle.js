7b1cb0bc8179f285e98d6a270d5b4731c78dd362
Saif Elhady <45790420+saif-elhady@users.noreply.github.com>
2024-04-17 19:46:10 +0200

    Create Vehicle.js

---
+++ Vehicle.js
@@ -0,0 +1,13 @@
+class Vehicle {
+  constructor(color = 'blue', wheels = 4, horn = 'beep beep') {
+    this.color = color;
+    this.wheels = wheels;
+    this.horn = horn;
+  }
+
+  honkHorn() {
+    console.log(this.horn);
+  }
+}
+
+module.exports = Vehicle;
