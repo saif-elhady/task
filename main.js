cdc846a115d6521db6c947c81a6e74a5131299e0
Saif Elhady <45790420+saif-elhady@users.noreply.github.com>
2024-04-17 19:47:20 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const Vehicle = require('./Vehicle.js');
+const Bicycle = require('./Bicycle.js');
+
+const myVehicle = new Vehicle();
+myVehicle.honkHorn(); 
+
+const myBicycle = new Bicycle();
+myBicycle.honkHorn();
